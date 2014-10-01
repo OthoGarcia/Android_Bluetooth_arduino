3d12be785b767821775804ee68d25fadb5e7c4d9
OthoGarcia <othogar@gmail.com>
2014-10-01 13:49:09 -0300

    Class Bluetooth

--- Carrinho/src/com/example/carrinho/BlueTooth.java
+++ Carrinho/src/com/example/carrinho/BlueTooth.java
@@ -1,5 +1,110 @@
 package com.example.carrinho;
+import android.os.Bundle;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import java.util.Set;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class BlueTooth {
+public class BlueTooth extends Activity {
 
+   private static final int REQUEST_ENABLE_BT = 1;
+   private Button onBtn;
+   private Button offBtn;
+   private Button listBtn;
+   private Button findBtn;
+   private TextView text;
+   private BluetoothAdapter myBluetoothAdapter;
+   private Set<BluetoothDevice> pairedDevices;
+   private ListView myListView;
+   private ArrayAdapter<String> BTArrayAdapter;
+  
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_tela);
+      
+      // take an instance of BluetoothAdapter - Bluetooth radio
+      myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+	      
+	      
+	     
+	      
+	    
+	      findBtn = (Button)findViewById(R.id.blouetooth);
+	      findBtn.setOnClickListener(new OnClickListener() {
+	  		
+	  		@Override
+	  		public void onClick(View v) {
+	  			// TODO Auto-generated method stub
+	  			on(v);
+	  			
+	  		}
+	      });
+	    
+	     
+      
+   }
+
+   public void on(View view){
+      if (!myBluetoothAdapter.isEnabled()) {
+         Intent turnOnIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+         startActivityForResult(turnOnIntent, REQUEST_ENABLE_BT);
+
+         Toast.makeText(getApplicationContext(),"Bluetooth turned on" ,
+        		 Toast.LENGTH_LONG).show();
+      }
+      else{
+         Toast.makeText(getApplicationContext(),"Bluetooth is already on",
+        		 Toast.LENGTH_LONG).show();
+      }
+   }
+   
+   @Override
+   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	   // TODO Auto-generated method stub
+	   if(requestCode == REQUEST_ENABLE_BT){
+		   if(myBluetoothAdapter.isEnabled()) {
+			   text.setText("Status: Enabled");
+		   } else {   
+			   text.setText("Status: Disabled");
+		   }
+	   }
+   }
+   
+   
+   final BroadcastReceiver bReceiver = new BroadcastReceiver() {
+	    public void onReceive(Context context, Intent intent) {
+	        String action = intent.getAction();
+	        // When discovery finds a device
+	        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+	             // Get the BluetoothDevice object from the Intent
+	        	 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+	        	 // add the name and the MAC address of the object to the arrayAdapter
+	             BTArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+	             BTArrayAdapter.notifyDataSetChanged();
+	        }
+	    }
+	};
+	
+  
+   
+   @Override
+   protected void onDestroy() {
+	   // TODO Auto-generated method stub
+	   super.onDestroy();
+	   unregisterReceiver(bReceiver);
+   }
+		
 }
